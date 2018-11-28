2083e1a01c418024242b7863726e07167a2f9aaf
gimmepizza <benimadhavagrawal@gmail.com>
2018-11-28 20:08:04 +0530

    DataHazardEX debugged

--- getcarry.vhd
+++ getcarry.vhd
@@ -12,16 +12,14 @@ architecture behave of GetCarry is
       process(RR_gives_C,RR_C , EX_gives_C, EX_C, MA_gives_C, MA_C, WB_gives_C, WB_C, RR_valid_bit)
 			begin
 				if RR_valid_bit = '1' then
-					if RR_gives_C = '1' then
-						C_out <= RR_C;
-					elsif EX_gives_C = '1' then
+					if EX_gives_C = '1' then
 						C_out <= EX_C;
 					elsif MA_gives_C = '1' then
 						C_out <= MA_C;
 					elsif WB_gives_C = '1' then
 						C_out <= WB_C;
 					else
-						C_out <= '0';
+						C_out <= RR_C;
 					end if;
 				else
 						C_out <= '0';
